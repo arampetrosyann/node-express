b3a287b4dd74a073df3778a5c8c5d3be1951e432
arampetrosyan <0aramp@gmail.com>
2020-10-08 02:48:44 +0400

    [ci] add title for 404 page

--- src/app.js
+++ src/app.js
@@ -22,8 +22,8 @@ app.use(router);
 
 app.use("/api", apiEnd);
 
-app.use(function (req, res, nex) {
-  res.status(404).render("404", { url: req.url });
+app.use(function (req, res, next) {
+  res.status(404).render("404", { title: "Not Found", url: req.url });
 });
 
 app.listen(port);
